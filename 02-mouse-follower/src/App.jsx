a98910b758f855fbc4c71df6821aeda26094e44f
David Almonacid <david.almonacidf@gmail.com>
2023-01-28 11:32:28 -0500

    Breaking into components

--- 02-mouse-follower/src/App.jsx
+++ 02-mouse-follower/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 
-const App = () => {
+const FollowMouse = () => {
   const [enabled, setEnabled] = useState(false);
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
@@ -27,8 +27,14 @@ const App = () => {
     };
   }, [enabled]);
 
+  const handleClick = (event) => {
+    const { clientX, clientY } = event;
+    setPosition({ x: clientX, y: clientY });
+    setEnabled(!enabled);
+  };
+
   return (
-    <div className='app'>
+    <>
       <div
         className='custom-cursor'
         style={{
@@ -36,9 +42,17 @@ const App = () => {
         }}
       />
       <h1>Mouse Follower</h1>
-      <button onClick={() => setEnabled(!enabled)}>
+      <button onClick={handleClick}>
         {enabled ? 'deactivate' : 'activate'} mouse follower
       </button>
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <div className='app'>
+      <FollowMouse />
     </div>
   );
 };
