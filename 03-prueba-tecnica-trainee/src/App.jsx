dc07a856ac19f75ef87c75fca27fbf4b9a34571f
David Almonacid <david.almonacidf@gmail.com>
2023-06-15 12:47:37 -0500

    Solution to the challenge

--- 03-prueba-tecnica-trainee/src/App.jsx
+++ 03-prueba-tecnica-trainee/src/App.jsx
@@ -1,10 +1,29 @@
-import viteLogo from '/vite.svg';
+import { useEffect, useState } from 'react';
 
 const App = () => {
+  const [fact, setFact] = useState('');
+  const [firstWord, setFirstWord] = useState('');
+
+  useEffect(() => {
+    const getFact = async () => {
+      const response = await fetch('https://catfact.ninja/fact');
+      const data = await response.json();
+      const { fact } = data;
+
+      setFact(fact);
+      setFirstWord(fact.split(' ')[0]);
+    };
+    getFact();
+  }, []);
+
   return (
     <>
-      <h1>Hello world</h1>
-      <img src={viteLogo} alt='' />
+      <h1>Random facts about cats</h1>
+      <p>{fact}</p>
+      <img
+        src={`https://cataas.com/cat/says/${encodeURIComponent(firstWord)}`}
+        alt=''
+      />
     </>
   );
 };
