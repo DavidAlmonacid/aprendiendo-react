157d0236faa451701169c65e6fdc159636865938
David Almonacid <david.almonacidf@gmail.com>
2023-06-15 11:36:20 -0500

    Initial commit

---
+++ 03-prueba-tecnica-trainee/src/App.jsx
@@ -0,0 +1,12 @@
+import viteLogo from '/vite.svg';
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello world</h1>
+      <img src={viteLogo} alt='' />
+    </>
+  );
+};
+
+export default App;
