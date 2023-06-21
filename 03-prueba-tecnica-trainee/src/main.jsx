14cf87ec8ddf49a38f290c8fdbcc20bc53825884
David Almonacid <david.almonacidf@gmail.com>
2023-06-21 12:13:57 -0500

    Add strict mode

--- 03-prueba-tecnica-trainee/src/main.jsx
+++ 03-prueba-tecnica-trainee/src/main.jsx
@@ -1,5 +1,11 @@
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './style.css';
 
-createRoot(document.getElementById('app')).render(<App />);
+const root = createRoot(document.getElementById('app'));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
