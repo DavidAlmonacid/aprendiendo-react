178bec317d28892e1c5bf1f268cd3b0eaf512299
David Almonacid <david.almonacidf@gmail.com>
2023-06-29 15:04:02 -0500

    Add tests

---
+++ 03-prueba-tecnica-trainee/tests/example.spec.js
@@ -0,0 +1,18 @@
+// @ts-check
+import { expect, test } from '@playwright/test';
+
+const LOCALHOST_URL = 'http://localhost:5173/';
+const BASE_IMAGE_URL = 'https://cataas.com';
+
+test('App shows a random fact and an image', async ({ page }) => {
+  await page.goto(LOCALHOST_URL);
+
+  const text = await page.getByRole('paragraph');
+  const image = await page.getByRole('img');
+
+  const textContent = await text.textContent();
+  const imageSrc = await image.getAttribute('src');
+
+  await expect(textContent?.length).toBeGreaterThan(0);
+  await expect(imageSrc?.startsWith(BASE_IMAGE_URL)).toBeTruthy();
+});
