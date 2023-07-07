02357373c4b0d43bb4afe7260c380ac4d20cfcde
David Almonacid <david.almonacidf@gmail.com>
2023-07-07 17:48:14 -0500

    Retrieve the user's movie name

--- 04-buscador-peliculas/src/App.jsx
+++ 04-buscador-peliculas/src/App.jsx
@@ -5,16 +5,25 @@ import { useMovies } from './hooks/useMovies';
 const App = () => {
   const { movies } = useMovies();
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    const movieName = data.get('movieName');
+    console.log({ movieName });
+  };
+
   return (
     <div>
       <header>
         <h1>Your Best Film Finder</h1>
 
-        <form className='form'>
+        <form className='form' onSubmit={handleSubmit}>
           <input
+            name='movieName'
             className='form__input'
             type='search'
             placeholder='Spider-Man, The Flash, Transformers, ...'
+            required
           />
 
           <button type='submit'>Search</button>
