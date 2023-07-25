4d2dfec7a0ec342ed1152f33149168282ae9799c
David Almonacid <david.almonacidf@gmail.com>
2023-07-25 17:30:38 -0500

    Create useFilters custom hook

---
+++ 05-shopping-cart/src/hooks/useFilters.ts
@@ -0,0 +1,17 @@
+import { FiltersContext } from '@/context/FiltersContext.tsx';
+import type { Product } from '@/types';
+import { useContext } from 'react';
+
+export const useFilters = () => {
+  const { filters, setFilters } = useContext(FiltersContext);
+
+  const filterProducts = (products: Product[]) => {
+    return products.filter(
+      (product) =>
+        product.price >= filters.minPrice &&
+        (filters.category === 'all' || product.category === filters.category)
+    );
+  };
+
+  return { filterProducts, filters, setFilters };
+};
