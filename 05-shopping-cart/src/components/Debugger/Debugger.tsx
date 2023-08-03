f16be9a02708fd7eca8303f2496411bedb30e9a6
David Almonacid <david.almonacidf@gmail.com>
2023-08-03 11:43:59 -0500

    Improve Debugger component

--- 05-shopping-cart/src/components/Debugger/Debugger.tsx
+++ 05-shopping-cart/src/components/Debugger/Debugger.tsx
@@ -1,14 +1,21 @@
-import { useFilters } from '@/hooks/useFilters';
+import { useCart } from '@/hooks/useCart';
 import './Debugger.css';
 
 type DebuggerProps = {};
 
 const Debugger: React.FC<DebuggerProps> = ({}) => {
-  const { filters } = useFilters();
+  const { cartProducts } = useCart();
+
+  const cartProductsToDisplay = cartProducts.map((product) => {
+    const { title, quantity } = product;
+    return { title, quantity };
+  });
 
   return (
     <div className='debugger'>
-      <p>{JSON.stringify(filters, null, 2)}</p>
+      <label htmlFor='toggle-debugger'>Toggle debugger</label>
+      <input type='checkbox' id='toggle-debugger' hidden />
+      <p>{JSON.stringify(cartProductsToDisplay, null, 2)}</p>
     </div>
   );
 };
