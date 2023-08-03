2b8c237428dbb9ce339910971617801fdfbb4622
David Almonacid <david.almonacidf@gmail.com>
2023-08-03 17:15:50 -0500

    Add validations

--- 05-shopping-cart/src/components/Products/Products.tsx
+++ 05-shopping-cart/src/components/Products/Products.tsx
@@ -1,4 +1,4 @@
-import { AddToCartIcon } from '@/components';
+import { AddToCartIcon, RemoveFromCartIcon } from '@/components';
 import { useCart } from '@/hooks/useCart';
 import { Product } from '@/types';
 import './Products.css';
@@ -8,34 +8,51 @@ export type ProductsProps = {
 };
 
 const Products: React.FC<ProductsProps> = ({ products }) => {
-  const { addToCart } = useCart();
+  const { cartProducts, addToCart, removeFromCart } = useCart();
+
+  const checkProductInCart = (product: Product) => {
+    return cartProducts.some((cartProduct) => cartProduct.id === product.id);
+  };
 
   return (
     <section className='products'>
-      {products.map((product: Product) => (
-        <article key={product.id} className='product'>
-          <header>
-            <h3 className='product__title' title={product.title}>
-              {product.title}
-            </h3>
-          </header>
+      {products.map((product) => {
+        const isProductInCart = checkProductInCart(product);
 
-          <picture className='product__image-wrapper'>
-            <img
-              src={product.thumbnail}
-              alt={product.title}
-              className='product__image'
-            />
-          </picture>
+        return (
+          <article key={product.id} className='product'>
+            <header>
+              <h3 className='product__title' title={product.title}>
+                {product.title}
+              </h3>
+            </header>
 
-          <div className='product__price'>
-            <span>${product.price}</span>
-            <button onClick={() => addToCart(product)}>
-              <AddToCartIcon />
-            </button>
-          </div>
-        </article>
-      ))}
+            <picture className='product__image-wrapper'>
+              <img
+                src={product.thumbnail}
+                alt={product.title}
+                className='product__image'
+              />
+            </picture>
+
+            <div className='product__price'>
+              <span>${product.price}</span>
+              <button
+                className={`product__button ${
+                  isProductInCart ? 'product__button--remove' : ''
+                }`}
+                onClick={() => {
+                  isProductInCart
+                    ? removeFromCart(product)
+                    : addToCart(product);
+                }}
+              >
+                {isProductInCart ? <RemoveFromCartIcon /> : <AddToCartIcon />}
+              </button>
+            </div>
+          </article>
+        );
+      })}
     </section>
   );
 };
