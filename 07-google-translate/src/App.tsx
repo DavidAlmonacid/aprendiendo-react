a3652f1ca8800b0df209c9c6f2f112783772009d
David Almonacid <david.almonacidf@gmail.com>
2023-11-29 15:44:45 -0500

    Add LanguageSelector component and interchange button

--- 07-google-translate/src/App.tsx
+++ 07-google-translate/src/App.tsx
@@ -1,21 +1,38 @@
+import { InterchangeIcon } from './components/Icons.tsx';
+import { LanguageSelector } from './components/LanguageSelector.tsx';
+import { AUTO_LANGUAGE } from './constants.ts';
 import { useStore } from './hooks/useStore.ts';
 
 function App() {
-  const { fromLanguage, setFromLanguage } = useStore();
+  const { fromLanguage, interchangeLanguages } = useStore();
 
   return (
     <>
-      <h1>Google translate</h1>
+      <h1 className='mb-6 text-3xl text-center'>Google translate</h1>
 
-      <button
-        onClick={() => {
-          setFromLanguage('es');
-        }}
-      >
-        Cambiar a Español
-      </button>
+      <div className='container mx-auto grid gap-4 md:grid-cols-[1fr,auto,1fr]'>
+        <section className='flex-grow text-center'>
+          <h2>From</h2>
+          <LanguageSelector />
+        </section>
 
-      <span>{fromLanguage}</span>
+        <div>
+          <button
+            className='py-1.5 px-2.5 opacity-80 cursor-pointer disabled:opacity-60'
+            disabled={fromLanguage === AUTO_LANGUAGE}
+            onClick={() => {
+              interchangeLanguages();
+            }}
+          >
+            <InterchangeIcon />
+          </button>
+        </div>
+
+        <section className='flex-grow text-center'>
+          <h2>To</h2>
+          <LanguageSelector />
+        </section>
+      </div>
     </>
   );
 }
