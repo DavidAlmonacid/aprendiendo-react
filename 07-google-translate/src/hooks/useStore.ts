685425c56ff61b287dcb19ac6ad2450eff719968
David Almonacid <david.almonacidf@gmail.com>
2023-11-29 10:27:53 -0500

    Add useStore hook for managing state

---
+++ 07-google-translate/src/hooks/useStore.ts
@@ -0,0 +1,88 @@
+import { useReducer } from 'react';
+import type { Action, State } from '../types.d.ts';
+
+const initialState: State = {
+  fromLanguage: 'auto',
+  toLanguage: 'en',
+  text: '',
+  translatedText: '',
+  isTranslating: false
+};
+
+function reducer(state: State, action: Action) {
+  switch (action.type) {
+    case 'INTERCHANGE_LANGUAGES':
+      return {
+        ...state,
+        fromLanguage: state.toLanguage,
+        toLanguage: state.fromLanguage
+      };
+
+    case 'SET_FROM_LANGUAGE':
+      return {
+        ...state,
+        fromLanguage: action.payload
+      };
+
+    case 'SET_TO_LANGUAGE':
+      return {
+        ...state,
+        toLanguage: action.payload
+      };
+
+    case 'SET_TEXT':
+      return {
+        ...state,
+        text: action.payload,
+        isTranslating: true,
+        translatedText: ''
+      };
+
+    case 'SET_TRANSLATED_TEXT':
+      return {
+        ...state,
+        translatedText: action.payload,
+        isTranslating: false
+      };
+  }
+}
+
+export function useStore() {
+  const [
+    { fromLanguage, toLanguage, text, translatedText, isTranslating },
+    dispatch
+  ] = useReducer(reducer, initialState);
+
+  const interchangeLanguages = () => {
+    dispatch({ type: 'INTERCHANGE_LANGUAGES' });
+  };
+
+  const setFromLanguage = (payload: string) => {
+    dispatch({ type: 'SET_FROM_LANGUAGE', payload });
+  };
+
+  const setToLanguage = (payload: string) => {
+    dispatch({ type: 'SET_TO_LANGUAGE', payload });
+  };
+
+  const setText = (payload: string) => {
+    dispatch({ type: 'SET_TEXT', payload });
+  };
+
+  const setTranslatedText = (payload: string) => {
+    dispatch({ type: 'SET_TRANSLATED_TEXT', payload });
+  };
+
+  return {
+    fromLanguage,
+    toLanguage,
+    text,
+    translatedText,
+    isTranslating,
+    interchangeLanguages,
+    setFromLanguage,
+    setToLanguage,
+    setText,
+    setTranslatedText
+  };
+}
