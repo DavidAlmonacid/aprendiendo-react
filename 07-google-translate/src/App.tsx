667f489d976b8130b06868deeab9b3f1b25b122a
David Almonacid <david.almonacidf@gmail.com>
2023-11-29 17:15:58 -0500

    Update language selectors in App component

--- 07-google-translate/src/App.tsx
+++ 07-google-translate/src/App.tsx
@@ -2,18 +2,29 @@ import { InterchangeIcon } from './components/Icons.tsx';
 import { LanguageSelector } from './components/LanguageSelector.tsx';
 import { AUTO_LANGUAGE } from './constants.ts';
 import { useStore } from './hooks/useStore.ts';
+import { SelectorType } from './types.d';
 
 function App() {
-  const { fromLanguage, interchangeLanguages } = useStore();
+  const {
+    fromLanguage,
+    toLanguage,
+    interchangeLanguages,
+    setFromLanguage,
+    setToLanguage
+  } = useStore();
 
   return (
     <>
       <h1 className='mb-6 text-3xl text-center'>Google translate</h1>
 
-      <div className='container mx-auto grid gap-4 md:grid-cols-[1fr,auto,1fr]'>
-        <section className='flex-grow text-center'>
-          <h2>From</h2>
-          <LanguageSelector />
+      <div className='container mx-auto grid justify-items-center gap-4 md:grid-cols-[1fr,auto,1fr]'>
+        <section className='flex-grow text-center w-full'>
+          <LanguageSelector
+            type={SelectorType.FROM}
+            value={fromLanguage}
+            onChange={setFromLanguage}
+          />
+          {fromLanguage}
         </section>
 
         <div>
@@ -28,9 +39,13 @@ function App() {
           </button>
         </div>
 
-        <section className='flex-grow text-center'>
-          <h2>To</h2>
-          <LanguageSelector />
+        <section className='flex-grow text-center w-full'>
+          <LanguageSelector
+            type={SelectorType.TO}
+            value={toLanguage}
+            onChange={setToLanguage}
+          />
+          {toLanguage}
         </section>
       </div>
     </>
