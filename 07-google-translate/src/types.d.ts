391023dd33781d8adf5885fa13a1fe62e74550fc
David Almonacid <david.almonacidf@gmail.com>
2023-11-29 15:43:43 -0500

    Add constants for supported languages and auto language
    Add type definitions for languages

--- 07-google-translate/src/types.d.ts
+++ 07-google-translate/src/types.d.ts
@@ -1,3 +1,5 @@
+import { AUTO_LANGUAGE, SUPPORTED_LANGUAGES } from './constants.ts';
+
 export interface State {
   fromLanguage: string;
   toLanguage: string;
@@ -12,3 +14,9 @@ export type Action =
   | { type: 'SET_TO_LANGUAGE'; payload: string }
   | { type: 'SET_TEXT'; payload: string }
   | { type: 'SET_TRANSLATED_TEXT'; payload: string };
+
+export type Language = keyof typeof SUPPORTED_LANGUAGES;
+
+export type AutoLanguage = typeof AUTO_LANGUAGE;
+
+export type FromLanguage = Languages | AutoLanguage;
