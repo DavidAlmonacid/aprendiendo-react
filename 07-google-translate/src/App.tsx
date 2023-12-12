e9f9c5b2c45788130f026013df0d87f18000aa19
David Almonacid <david.almonacidf@gmail.com>
2023-12-12 17:26:32 -0500

    Add TextArea component

--- 07-google-translate/src/App.tsx
+++ 07-google-translate/src/App.tsx
@@ -1,5 +1,6 @@
 import { InterchangeIcon } from './components/Icons.tsx';
 import { LanguageSelector } from './components/LanguageSelector.tsx';
+import { TextArea } from './components/TextArea.tsx';
 import { AUTO_LANGUAGE } from './constants.ts';
 import { useStore } from './hooks/useStore.ts';
 import { SelectorType } from './types.d';
@@ -8,23 +9,28 @@ function App() {
   const {
     fromLanguage,
     toLanguage,
+    text,
+    translatedText,
+    isTranslating,
     interchangeLanguages,
     setFromLanguage,
-    setToLanguage
+    setToLanguage,
+    setText,
+    setTranslatedText
   } = useStore();
 
   return (
     <>
       <h1 className='mb-6 text-3xl text-center'>Google translate</h1>
 
-      <div className='container mx-auto grid justify-items-center gap-4 md:grid-cols-[1fr,auto,1fr] max-w-6xl'>
+      <div className='container mx-auto grid justify-items-center gap-4 md:grid-cols-[1fr,auto,1fr] max-w-6xl px-10'>
         <section className='flex-grow text-center w-full'>
           <LanguageSelector
             type={SelectorType.FROM}
             value={fromLanguage}
             onChange={setFromLanguage}
           />
-          {fromLanguage}
+          <TextArea type={SelectorType.FROM} value={text} onChange={setText} />
         </section>
 
         <div>
@@ -45,7 +51,12 @@ function App() {
             value={toLanguage}
             onChange={setToLanguage}
           />
-          {toLanguage}
+          <TextArea
+            type={SelectorType.TO}
+            value={translatedText}
+            isTranslating={isTranslating}
+            onChange={setTranslatedText}
+          />
         </section>
       </div>
     </>
