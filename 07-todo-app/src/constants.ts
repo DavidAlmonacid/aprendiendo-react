4fbf6d80c44069f31ae6480aca1f1e21d70d31b7
David Almonacid <david.almonacidf@gmail.com>
2024-01-26 12:17:49 -0500

    Refactor constants in todo app

--- 07-todo-app/src/constants.ts
+++ 07-todo-app/src/constants.ts
@@ -1,7 +1,28 @@
 import type { Todo } from "./types";
 
-export const initialTodos: Todo[] = [
+export const INITIAL_TODOS: Todo[] = [
   { id: "1", title: "Aprender React", completed: false },
   { id: "2", title: "Aprender TypeScript", completed: true },
   { id: "3", title: "Aprender Vite", completed: false }
 ];
+
+export const TODO_FILTERS = {
+  ALL: "all",
+  ACTIVE: "active",
+  COMPLETED: "completed"
+} as const;
+
+export const FILTER_BUTTONS = {
+  [TODO_FILTERS.ALL]: {
+    label: "All",
+    herf: `/?filter=${TODO_FILTERS.ALL}`
+  },
+  [TODO_FILTERS.ACTIVE]: {
+    label: "Active",
+    herf: `/?filter=${TODO_FILTERS.ACTIVE}`
+  },
+  [TODO_FILTERS.COMPLETED]: {
+    label: "Completed",
+    herf: `/?filter=${TODO_FILTERS.COMPLETED}`
+  }
+} as const;
