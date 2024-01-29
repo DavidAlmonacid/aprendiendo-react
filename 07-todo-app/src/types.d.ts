5d40ca4c1f1c636bf7833ff4c0d2e2bed14cee59
David Almonacid <david.almonacidf@gmail.com>
2024-01-29 15:32:43 -0500

    Add FilterValue type

--- 07-todo-app/src/types.d.ts
+++ 07-todo-app/src/types.d.ts
@@ -1,3 +1,5 @@
+import type { TODO_FILTERS } from "./constants.ts";
+
 export interface Todo {
   id: string;
   title: string;
@@ -5,3 +7,5 @@ export interface Todo {
 }
 
 export type TodoId = Pick<Todo, "id">;
+
+export type FilterValue = (typeof TODO_FILTERS)[keyof typeof TODO_FILTERS];
