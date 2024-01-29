45d030c670e6df5ef00521e677db5a0fb807f918
David Almonacid <david.almonacidf@gmail.com>
2024-01-29 16:12:15 -0500

    Add TodosProvider to main.tsx

--- 07-todo-app/src/main.tsx
+++ 07-todo-app/src/main.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { TodosProvider } from "./contexts/TodosContext.tsx";
 
 import "todomvc-app-css/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <TodosProvider>
+      <App />
+    </TodosProvider>
   </React.StrictMode>
 );
