c6cb1139d03a3c424969048cd9b984d5d0e8e6fe
David Almonacid <david.almonacidf@gmail.com>
2024-01-29 16:10:34 -0500

    Add initial todos to TodosContext

--- 07-todo-app/src/contexts/TodosContext.tsx
+++ 07-todo-app/src/contexts/TodosContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, useState } from "react";
+import { INITIAL_TODOS } from "../constants.ts";
 import type { Todo } from "../types.d.ts";
 
 interface TodosContextProps {
@@ -9,7 +10,7 @@ interface TodosContextProps {
 export const TodosContext = createContext({} as TodosContextProps);
 
 export function TodosProvider({ children }: { children: React.ReactNode }) {
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todos, setTodos] = useState<Todo[]>(INITIAL_TODOS);
 
   return (
     <TodosContext.Provider value={{ todos, setTodos }}>
