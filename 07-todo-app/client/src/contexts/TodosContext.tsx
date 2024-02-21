677cac217f713b26b3c62071cdc9f51c3cfafccc
David Almonacid <david.almonacidf@gmail.com>
2024-02-21 18:04:46 -0500

    Update TodosContext to fetch initial todos from API

--- 07-todo-app/client/src/contexts/TodosContext.tsx
+++ 07-todo-app/client/src/contexts/TodosContext.tsx
@@ -1,6 +1,8 @@
 import { createContext, useState } from "react";
-import { INITIAL_TODOS } from "../constants.ts";
+import { fetchTasks } from "../api/tasks.ts";
 import type { Todo } from "../types.d.ts";
+
+const initialTodos: Todo[] = await fetchTasks();
 
 interface TodosContextProps {
   todos: Todo[];
@@ -10,7 +12,7 @@ interface TodosContextProps {
 export const TodosContext = createContext({} as TodosContextProps);
 
 export function TodosProvider({ children }: { children: React.ReactNode }) {
-  const [todos, setTodos] = useState<Todo[]>(INITIAL_TODOS);
+  const [todos, setTodos] = useState(initialTodos);
 
   return (
     <TodosContext.Provider value={{ todos, setTodos }}>
