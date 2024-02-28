34722926ed02476cd262866b10b7296f1834cc33
David Almonacid <david.almonacidf@gmail.com>
2024-02-28 11:55:51 -0500

    Add language selection and interchange functionality

--- 08-google-translate/client/src/App.tsx
+++ 08-google-translate/client/src/App.tsx
@@ -1,8 +1,36 @@
+import { ArrowsSwap } from "./components/Icons.tsx";
+import { useStore } from "./hooks/useStore.ts";
+import { AUTO_LANGUAGE } from "./types/constants.ts";
+
 function App() {
+  const { fromLanguage, toLanguage, interchangeLanguages } = useStore();
+
   return (
-    <>
-      <h1>Hello, World!</h1>
-    </>
+    <div>
+      <h1>Google Translate</h1>
+
+      <div className="grid grid-cols-[1fr_min-content_1fr] max-w-3xl mx-auto">
+        <section>
+          <h2>from</h2>
+          {fromLanguage}
+        </section>
+
+        <div>
+          <button
+            disabled={fromLanguage === AUTO_LANGUAGE}
+            className="disabled:opacity-40 disabled:cursor-auto"
+            onClick={() => interchangeLanguages()}
+          >
+            <ArrowsSwap />
+          </button>
+        </div>
+
+        <section>
+          <h2>to</h2>
+          {toLanguage}
+        </section>
+      </div>
+    </div>
   );
 }
 
