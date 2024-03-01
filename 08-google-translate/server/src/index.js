0697d277754bc690b72906887fc7009506b8acfb
David Almonacid <david.almonacidf@gmail.com>
2024-03-01 15:04:06 -0500

    Add CORS middleware to server

--- 08-google-translate/server/src/index.js
+++ 08-google-translate/server/src/index.js
@@ -1,9 +1,13 @@
-import "dotenv/config";
+import cors from "cors";
 import express from "express";
+
 import { translateRouter } from "./routes/translate.js";
+
+import "dotenv/config";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use("/api/translate", translateRouter);
 
 const PORT = process.env.PORT || 4000;
