b87d575e82ddfc9af7dfe5fab655c711e0dccdd1
David Almonacid <david.almonacidf@gmail.com>
2024-03-11 07:57:44 -0500

    Add config for use tests

--- 08-google-translate/client/vite.config.ts
+++ 08-google-translate/client/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from "vite";
+/// <reference types="vitest" />
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  test: {
+    environment: "happy-dom"
+  }
 });
