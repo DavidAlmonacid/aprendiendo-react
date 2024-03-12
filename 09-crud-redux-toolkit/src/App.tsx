e2e7b2024eb024d09f0be7c13f8c2fcefec5fd2d
David Almonacid <david.almonacidf@gmail.com>
2024-03-12 15:14:34 -0500

    Add ListOfUsers component to App.tsx

--- 09-crud-redux-toolkit/src/App.tsx
+++ 09-crud-redux-toolkit/src/App.tsx
@@ -1,7 +1,10 @@
+import { ListOfUsers } from "./components/ListOfUsers.tsx";
+
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <ListOfUsers />
     </>
   );
 }
