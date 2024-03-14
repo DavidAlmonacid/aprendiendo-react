23db7546d598d41726b25f8ba6919ebf0d95f83d
David Almonacid <david.almonacidf@gmail.com>
2024-03-14 10:39:42 -0500

    Add persistance local storage middleware to store middleware

--- 09-crud-redux-toolkit/src/store/index.ts
+++ 09-crud-redux-toolkit/src/store/index.ts
@@ -1,10 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from "./users/slice.ts";
 
+const persistanceLocalStorageMiddleware = (store) => (next) => (action) => {
+  next(action);
+  localStorage.setItem("users_redux_state", JSON.stringify(store.getState()));
+};
+
 export const store = configureStore({
   reducer: {
     users: usersReducer
-  }
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(persistanceLocalStorageMiddleware)
 });
 
 export type RootState = ReturnType<typeof store.getState>;
