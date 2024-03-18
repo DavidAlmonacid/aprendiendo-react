2904191f3b036ad456c66c328d952e68c19cae41
David Almonacid <david.almonacidf@gmail.com>
2024-03-18 12:38:44 -0500

    Add addUser function to useUserActions hook

--- 09-crud-redux-toolkit/src/hooks/useUserActions.ts
+++ 09-crud-redux-toolkit/src/hooks/useUserActions.ts
@@ -1,13 +1,17 @@
-import { deleteUserById } from "../store/users/slice.ts";
-import type { UserId } from "../types/types.d.ts";
+import { addNewUser, deleteUserById } from "../store/users/slice.ts";
+import type { User, UserId } from "../types/types.d.ts";
 import { useAppDispatch } from "./store";
 
 export function useUserActions() {
   const dispatch = useAppDispatch();
 
+  const addUser = ({ name, email, github }: User) => {
+    dispatch(addNewUser({ name, email, github }));
+  };
+
   const deleteUser = (id: UserId) => {
     dispatch(deleteUserById(id));
   };
 
-  return { deleteUser };
+  return { addUser, deleteUser };
 }
