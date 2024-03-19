d9070e3d0010e33ac5b33bc056b7a4248a2e52c9
David Almonacid <david.almonacidf@gmail.com>
2024-03-19 16:39:50 -0500

    Refactor CreateNewUser component to handle form submission and display result

--- 09-crud-redux-toolkit/src/components/CreateNewUser.tsx
+++ 09-crud-redux-toolkit/src/components/CreateNewUser.tsx
@@ -1,11 +1,16 @@
-import { Button, Card, TextInput, Title } from "@tremor/react";
+import { useState } from "react";
+
+import { Badge, Button, Card, TextInput, Title } from "@tremor/react";
 import { useUserActions } from "../hooks/useUserActions.ts";
 
 export function CreateNewUser() {
+  const [result, setResult] = useState<"ok" | "ko" | null>(null);
   const { addUser } = useUserActions();
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    setResult(null);
 
     const form = event.currentTarget;
     const formData = new FormData(form);
@@ -14,7 +19,14 @@ export function CreateNewUser() {
     const email = formData.get("email") as string;
     const github = formData.get("github") as string;
 
+    if (!name || !email || !github) {
+      setResult("ko");
+      return;
+    }
+
     addUser({ name, email, github });
+    setResult("ok");
+    form.reset();
   };
 
   return (
@@ -23,11 +35,15 @@ export function CreateNewUser() {
 
       <form onSubmit={handleSubmit}>
         <TextInput placeholder="Name" name="name" />
-        <TextInput placeholder="Email" name="email" />
+        <TextInput placeholder="Email" name="email" type="email" />
         <TextInput placeholder="GitHub username" name="github" />
 
         <div>
           <Button>Create</Button>
+          <span>
+            {result === "ok" && <Badge color="green">User created</Badge>}
+            {result === "ko" && <Badge color="red">Invalid input</Badge>}
+          </span>
         </div>
       </form>
     </Card>
