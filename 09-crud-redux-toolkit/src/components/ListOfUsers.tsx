13f1ce3b1c291e5fa91a9236e9a1760ecd4c6a49
David Almonacid <david.almonacidf@gmail.com>
2024-03-20 16:37:05 -0500

    Refactor ListOfUsers component UI

--- 09-crud-redux-toolkit/src/components/ListOfUsers.tsx
+++ 09-crud-redux-toolkit/src/components/ListOfUsers.tsx
@@ -1,69 +1,135 @@
-import {
-  Badge,
-  Card,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeaderCell,
-  TableRow,
-  Title
-} from "@tremor/react";
-import { EditIcon, TrashIcon } from "./Icons.tsx";
-
 import { useAppSelector } from "../hooks/store.ts";
 import { useUserActions } from "../hooks/useUserActions.ts";
+import { EditIcon, TrashIcon } from "./Icons.tsx";
 
 export function ListOfUsers() {
   const users = useAppSelector((state) => state.users);
   const { deleteUser } = useUserActions();
 
   return (
-    <Card>
-      <Title>
-        <span>Users</span>
-        <Badge>{users.length}</Badge>
-      </Title>
+    <div className="px-4">
+      <div className="shadow-md sm:rounded-2xl max-w-3xl mx-auto overflow-hidden">
+        <section className="flex items-center justify-between flex-column flex-wrap md:flex-row gap-4 bg-white sm:bg-gray-900 sm:px-4 py-3">
+          <div>
+            <button
+              id="dropdownActionButton"
+              data-dropdown-toggle="dropdownAction"
+              className="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+              type="button"
+            >
+              <span className="sr-only">Action button</span>
+              Action
+              <svg
+                className="w-2.5 h-2.5 ms-2.5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 10 6"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="m1 1 4 4 4-4"
+                />
+              </svg>
+            </button>
+          </div>
 
-      <Table className="mt-5">
-        <TableHead>
-          <TableRow>
-            <TableHeaderCell>ID</TableHeaderCell>
-            <TableHeaderCell>Name</TableHeaderCell>
-            <TableHeaderCell>Email</TableHeaderCell>
-            <TableHeaderCell>Actions</TableHeaderCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {users.map((user) => (
-            <TableRow key={user.name}>
-              <TableCell>
-                {user.id.length > 4 ? user.id.slice(0, 4) + "..." : user.id}
-              </TableCell>
-              <TableCell className="flex items-center gap-2">
-                <picture>
-                  <img
-                    src={`https://unavatar.io/github/${user.github}`}
-                    alt={user.name}
-                    className="w-8 h-8 rounded-full"
-                  />
-                </picture>
-                <span>{user.name}</span>
-              </TableCell>
-              <TableCell>{user.email}</TableCell>
-              <TableCell>
-                <button>
-                  <EditIcon />
-                </button>
+          <label htmlFor="table-search-users" className="sr-only">
+            Search
+          </label>
+          <div className="relative">
+            <div className="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg
+                className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
+              </svg>
+            </div>
+            <input
+              type="text"
+              id="table-search-users"
+              className="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Search for users"
+            />
+          </div>
+        </section>
 
-                <button onClick={() => deleteUser(user.id)}>
-                  <TrashIcon />
-                </button>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </Card>
+        <div className="w-full overflow-x-auto">
+          <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" className="p-4">
+                  ID
+                </th>
+
+                <th scope="col" className="px-6 py-3">
+                  Name
+                </th>
+
+                <th scope="col" className="px-6 py-3">
+                  Position
+                </th>
+
+                <th scope="col" className="px-6 py-3">
+                  Actions
+                </th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {users.map((user, index) => (
+                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                  <td className="w-4 p-4">
+                    {user.id.length > 4 ? user.id.slice(0, 4) + "..." : user.id}
+                  </td>
+
+                  <th
+                    scope="row"
+                    className="flex items-center gap-x-3 px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    <img
+                      className="w-10 h-10 rounded-full"
+                      src={`https://picsum.photos/100/100?random=${index + 1}`}
+                      alt={`Profile picture of ${user.name}`}
+                    />
+                    <div>
+                      <div className="text-base font-semibold">{user.name}</div>
+                      <div className="font-normal text-gray-500">
+                        {user.email}
+                      </div>
+                    </div>
+                  </th>
+
+                  <td className="px-6 py-4">{user.role}</td>
+
+                  <td className="px-6 py-4">
+                    <button>
+                      <EditIcon />
+                    </button>
+
+                    <button onClick={() => deleteUser(user.id)}>
+                      <TrashIcon />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   );
 }
