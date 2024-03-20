b7c022c38c8c2f73bcecc11dd6c106e717a55b58
David Almonacid <david.almonacidf@gmail.com>
2024-03-20 17:38:20 -0500

    Make default dark mode for the ListOfUsers component

--- 09-crud-redux-toolkit/src/components/ListOfUsers.tsx
+++ 09-crud-redux-toolkit/src/components/ListOfUsers.tsx
@@ -15,7 +15,7 @@ export function ListOfUsers() {
 
             <span>
               Total&nbsp;
-              <span className="bg-gray-100 text-sm font-medium px-2.5 py-0.5 rounded dark:bg-gray-700">
+              <span className="text-sm font-medium px-2.5 py-0.5 rounded bg-gray-700">
                 {users.length}
               </span>
             </span>
@@ -23,17 +23,17 @@ export function ListOfUsers() {
 
           <div className="flex justify-between gap-x-4">
             <button
-              className="inline-flex text-nowrap items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+              className="inline-flex text-nowrap items-center text-gray-400 bg-gray-800 border border-gray-600 focus:outline-none hover:bg-gray-700 focus:ring-4 focus:ring-gray-700 font-medium rounded-lg text-sm px-3 py-1.5 hover:border-gray-600"
               type="button"
             >
               Add New User
             </button>
 
             <label className="sr-only">Search</label>
-            <div className="flex items-center border border-gray-300 rounded-lg dark:border-gray-600 dark:bg-gray-700 overflow-hidden">
+            <div className="flex items-center border border-gray-600 rounded-lg bg-gray-700 overflow-hidden">
               <span className="px-2">
                 <svg
-                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                  className="w-4 h-4 text-gray-400"
                   aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -50,7 +50,7 @@ export function ListOfUsers() {
               </span>
               <input
                 type="text"
-                className="p-2 text-sm text-gray-900 w-80 bg-transparent dark:placeholder-gray-400 dark:text-white outline-none"
+                className="p-2 text-sm text-white w-80 bg-transparent placeholder-gray-400 outline-none"
                 placeholder="Search for users"
               />
             </div>
@@ -58,8 +58,8 @@ export function ListOfUsers() {
         </section>
 
         <div className="w-full overflow-x-auto">
-          <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <table className="w-full text-sm text-left rtl:text-right text-gray-400">
+            <thead className="text-xs text-gray-400 uppercase bg-gray-700">
               <tr>
                 <th scope="col" className="p-4">
                   ID
@@ -81,14 +81,14 @@ export function ListOfUsers() {
 
             <tbody>
               {users.map((user, index) => (
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <tr className="bg-gray-800 border-b border-gray-700 hover:bg-gray-600">
                   <td className="w-4 p-4">
                     {user.id.length > 4 ? user.id.slice(0, 4) + "..." : user.id}
                   </td>
 
                   <th
                     scope="row"
-                    className="flex items-center gap-x-3 px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white"
+                    className="flex items-center gap-x-3 px-6 py-4 text-white whitespace-nowrap"
                   >
                     <img
                       className="w-10 h-10 rounded-full"
