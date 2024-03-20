009a970a69a1fa9509235a2052443ad1c29677e1
David Almonacid <david.almonacidf@gmail.com>
2024-03-20 16:19:27 -0500

    Remove h1 element from App component

--- 09-crud-redux-toolkit/src/App.tsx
+++ 09-crud-redux-toolkit/src/App.tsx
@@ -4,7 +4,6 @@ import { ListOfUsers } from "./components/ListOfUsers.tsx";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <ListOfUsers />
       <CreateNewUser />
     </>
