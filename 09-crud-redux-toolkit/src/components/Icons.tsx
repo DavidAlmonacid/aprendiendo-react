594c5b98ef3ef2b75f918ff4865a4222bdaebd33
David Almonacid <david.almonacidf@gmail.com>
2024-03-21 10:27:52 -0500

    Add SearchIcon component to Icons.tsx

--- 09-crud-redux-toolkit/src/components/Icons.tsx
+++ 09-crud-redux-toolkit/src/components/Icons.tsx
@@ -31,3 +31,21 @@ export const EditIcon = () => (
     />
   </svg>
 );
+
+export const SearchIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 20 20"
+    stroke="currentColor"
+    className="size-4"
+  >
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+    />
+  </svg>
+);
