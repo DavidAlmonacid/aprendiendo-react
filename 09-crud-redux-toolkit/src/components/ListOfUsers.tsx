dd62e0590ece2ed910c763d1bfac9089fdf184fa
David Almonacid <david.almonacidf@gmail.com>
2024-03-21 17:24:32 -0500

    Add CreateNewUser modal functionality

--- 09-crud-redux-toolkit/src/components/ListOfUsers.tsx
+++ 09-crud-redux-toolkit/src/components/ListOfUsers.tsx
@@ -1,16 +1,28 @@
+import { useState } from "react";
 import { useAppSelector } from "../hooks/store.ts";
 import { useUserActions } from "../hooks/useUserActions.ts";
+import type { UserId } from "../types/types";
+import { CreateNewUser } from "./CreateNewUser.tsx";
 import { EditIcon, SearchIcon, TrashIcon } from "./Icons.tsx";
 
 export function ListOfUsers() {
+  const [isOpenModal, setIsOpenModal] = useState(false);
   const users = useAppSelector((state) => state.users);
   const { deleteUser } = useUserActions();
+
+  const handleOpenModal = () => {
+    setIsOpenModal(true);
+  };
+
+  const handleDeleteUser = (id: UserId) => {
+    deleteUser(id);
+  };
 
   return (
     <div className="flex items-center px-4 min-h-screen">
       <div className="max-w-3xl mx-auto py-16 w-full overflow-hidden">
-        <section className="flex flex-col gap-y-4 bg-gray-900 px-4 py-3 rounded-t-2xl">
-          <header className="flex items-end justify-between gap-x-2 text-gray-300">
+        <header className="flex flex-col gap-y-4 bg-gray-900 px-4 py-3 rounded-t-2xl">
+          <section className="flex items-end justify-between gap-x-2 text-gray-300">
             <h3 className="text-2xl font-semibold leading-none">Users</h3>
 
             <span>
@@ -19,7 +31,7 @@ export function ListOfUsers() {
                 {users.length}
               </span>
             </span>
-          </header>
+          </section>
 
           <div className="flex justify-between gap-x-4">
             <div className="flex items-center border border-gray-600 rounded-lg bg-gray-700 overflow-hidden">
@@ -37,11 +49,16 @@ export function ListOfUsers() {
             <button
               className="inline-flex text-nowrap items-center text-gray-400 bg-gray-800 border border-gray-600 focus:outline-none hover:bg-gray-700 focus:ring-4 focus:ring-gray-700 font-medium rounded-lg text-sm px-3 py-1.5 hover:border-gray-600"
               type="button"
+              onClick={handleOpenModal}
             >
               Add New User
             </button>
+
+            {isOpenModal && (
+              <CreateNewUser open={isOpenModal} setOpen={setIsOpenModal} />
+            )}
           </div>
-        </section>
+        </header>
 
         <div className="w-full overflow-x-auto rounded-b-2xl shadow-md">
           <table className="w-full text-sm text-left rtl:text-right text-gray-400">
@@ -103,7 +120,7 @@ export function ListOfUsers() {
                       <button
                         type="button"
                         aria-label="Delete user"
-                        onClick={() => deleteUser(user.id)}
+                        onClick={() => handleDeleteUser(user.id)}
                       >
                         <TrashIcon />
                       </button>
