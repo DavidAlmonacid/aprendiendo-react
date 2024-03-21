b62476c9eda67b025334c431bc781b5cbc559ac3
David Almonacid <david.almonacidf@gmail.com>
2024-03-21 10:28:52 -0500

    Add SearchIcon component

--- 09-crud-redux-toolkit/src/components/ListOfUsers.tsx
+++ 09-crud-redux-toolkit/src/components/ListOfUsers.tsx
@@ -1,6 +1,6 @@
 import { useAppSelector } from "../hooks/store.ts";
 import { useUserActions } from "../hooks/useUserActions.ts";
-import { EditIcon, TrashIcon } from "./Icons.tsx";
+import { EditIcon, SearchIcon, TrashIcon } from "./Icons.tsx";
 
 export function ListOfUsers() {
   const users = useAppSelector((state) => state.users);
@@ -31,22 +31,8 @@ export function ListOfUsers() {
 
             <label className="sr-only">Search</label>
             <div className="flex items-center border border-gray-600 rounded-lg bg-gray-700 overflow-hidden">
-              <span className="px-2">
-                <svg
-                  className="w-4 h-4 text-gray-400"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                  />
-                </svg>
+              <span className="px-2 text-gray-400">
+                <SearchIcon />
               </span>
               <input
                 type="text"
