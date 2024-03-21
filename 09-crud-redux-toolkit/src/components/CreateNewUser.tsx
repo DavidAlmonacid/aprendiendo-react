8606198cabde57feda0f9c100af56c96977631a7
David Almonacid <david.almonacidf@gmail.com>
2024-03-21 17:23:47 -0500

    Refactor CreateNewUser component UI and add modal functionality

--- 09-crud-redux-toolkit/src/components/CreateNewUser.tsx
+++ 09-crud-redux-toolkit/src/components/CreateNewUser.tsx
@@ -1,11 +1,21 @@
-import { useState } from "react";
-
-import { Badge, Button, Card, TextInput, Title } from "@tremor/react";
+import { useId, useState } from "react";
 import { useUserActions } from "../hooks/useUserActions.ts";
 
-export function CreateNewUser() {
+interface Props {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export function CreateNewUser({ open = false, setOpen }: Props) {
   const [result, setResult] = useState<"ok" | "ko" | null>(null);
+  const id = useId();
   const { addUser } = useUserActions();
+
+  const handleCloseModal = () => {
+    const modal = document.getElementById(id) as HTMLDialogElement;
+    modal.close();
+    setOpen(false);
+  };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -19,7 +29,10 @@ export function CreateNewUser() {
     const email = formData.get("email") as string;
     const role = formData.get("role") as string;
 
-    if (!name || !email || !role) {
+    const emailPattern = /^[\w.%+-]+@example\.com$/;
+    const isEmailValid = emailPattern.test(email);
+
+    if (!name || !isEmailValid || !role) {
       setResult("ko");
       return;
     }
@@ -30,22 +43,132 @@ export function CreateNewUser() {
   };
 
   return (
-    <Card>
-      <Title>Create New User</Title>
+    <dialog
+      open={open}
+      id={id}
+      className="bg-white rounded-lg shadow dark:bg-gray-700"
+    >
+      <header className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+          Create New User
+        </h3>
 
-      <form onSubmit={handleSubmit}>
-        <TextInput placeholder="Name" name="name" />
-        <TextInput placeholder="Email" name="email" type="email" />
-        <TextInput placeholder="Role" name="role" />
+        <button
+          type="button"
+          className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white outline-none"
+          onClick={() => handleCloseModal()}
+          autoFocus
+        >
+          <svg
+            className="w-3 h-3"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 14"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+            />
+          </svg>
+          <span className="sr-only">Close modal</span>
+        </button>
+      </header>
 
-        <div>
-          <Button>Create</Button>
-          <span>
-            {result === "ok" && <Badge color="green">User created</Badge>}
-            {result === "ko" && <Badge color="red">Invalid input</Badge>}
+      <form className="p-4 md:p-5" onSubmit={handleSubmit}>
+        <section className="grid gap-4 mb-5">
+          <div>
+            <label
+              htmlFor="name"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              autoComplete="name"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+              placeholder="John Doe"
+              required
+            />
+          </div>
+
+          <div>
+            <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              autoComplete="email"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+              placeholder="@example.com"
+              pattern="\w+.\w+@example\.com"
+              title="Must end with &#34;@example.com&#34;"
+              required
+            />
+          </div>
+
+          <div>
+            <label
+              htmlFor="role"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Role
+            </label>
+            <input
+              type="text"
+              id="role"
+              name="role"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+              placeholder="Software Engineer"
+              required
+            />
+          </div>
+        </section>
+
+        <div className="grid gap-2">
+          <button
+            type="submit"
+            className="text-white flex justify-center items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            <svg
+              className="me-1 -ms-1 w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+            Create
+          </button>
+          <span className="self-start">
+            {result === "ok" && (
+              <span className="bg-green-100 text-green-800 text-xs font-medium px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-green-400 border border-green-400">
+                User created
+              </span>
+            )}
+            {result === "ko" && (
+              <span className="bg-red-100 text-red-800 text-xs font-medium px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-red-400 border border-red-400">
+                Invalid input
+              </span>
+            )}
           </span>
         </div>
       </form>
-    </Card>
+    </dialog>
   );
 }
