f1db53e83855076bce7dc64f83018f885e78d4c5
David Almonacid <david.almonacidf@gmail.com>
2024-03-22 15:46:44 -0500

    Make dark mode by default

--- 09-crud-redux-toolkit/src/components/CreateNewUser.tsx
+++ 09-crud-redux-toolkit/src/components/CreateNewUser.tsx
@@ -52,10 +52,8 @@ export function CreateNewUser({ open = false, setOpen }: Props) {
       id={id}
       className="rounded-lg shadow-md bg-gray-700 w-80 m-0 left-[calc(100%-20rem)]"
     >
-      <header className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-          Create New User
-        </h3>
+      <header className="flex items-center justify-between p-4 md:p-5 border-b rounded-t border-gray-600">
+        <h3 className="text-lg font-semibold text-white">Create New User</h3>
 
         <button
           type="button"
@@ -87,7 +85,7 @@ export function CreateNewUser({ open = false, setOpen }: Props) {
           <div>
             <label
               htmlFor="name"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              className="block mb-2 text-sm font-medium text-white"
             >
               Name
             </label>
@@ -96,7 +94,7 @@ export function CreateNewUser({ open = false, setOpen }: Props) {
               id="name"
               name="name"
               autoComplete="name"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+              className="border text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500"
               placeholder="John Doe"
               required
             />
@@ -105,7 +103,7 @@ export function CreateNewUser({ open = false, setOpen }: Props) {
           <div>
             <label
               htmlFor="email"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              className="block mb-2 text-sm font-medium text-white"
             >
               Email
             </label>
@@ -114,7 +112,7 @@ export function CreateNewUser({ open = false, setOpen }: Props) {
               id="email"
               name="email"
               autoComplete="email"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+              className="border text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500"
               placeholder="@example.com"
               pattern="\w+\.*\w*@example\.com"
               title="Must end with &#34;@example.com&#34;"
@@ -125,7 +123,7 @@ export function CreateNewUser({ open = false, setOpen }: Props) {
           <div>
             <label
               htmlFor="role"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              className="block mb-2 text-sm font-medium text-white"
             >
               Role
             </label>
@@ -133,7 +131,7 @@ export function CreateNewUser({ open = false, setOpen }: Props) {
               type="text"
               id="role"
               name="role"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+              className="border text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500"
               placeholder="Software Engineer"
               required
             />
@@ -143,7 +141,7 @@ export function CreateNewUser({ open = false, setOpen }: Props) {
         <div className="grid gap-2.5">
           <button
             type="submit"
-            className="text-white flex justify-center items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="text-white flex justify-center items-center focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
           >
             <svg
               className="me-1 -ms-1 w-5 h-5"
