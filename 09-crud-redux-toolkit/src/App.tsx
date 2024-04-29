45f39f4692bfe227d6abe6ef78021e94c74d84a7
David Almonacid <david.almonacidf@gmail.com>
2024-04-29 15:11:16 -0500

    feat: Add Toaster component for displaying rich colors and visible toasts

--- 09-crud-redux-toolkit/src/App.tsx
+++ 09-crud-redux-toolkit/src/App.tsx
@@ -1,9 +1,11 @@
+import { Toaster } from "sonner";
 import { ListOfUsers } from "./components/ListOfUsers.tsx";
 
 function App() {
   return (
     <>
       <ListOfUsers />
+      <Toaster richColors visibleToasts={1} />
     </>
   );
 }
