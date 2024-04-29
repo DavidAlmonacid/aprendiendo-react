fca76cf97168bb3c36764c11176dba9fe71b7452
David Almonacid <david.almonacidf@gmail.com>
2024-04-29 11:55:34 -0500

    chore: Add persistanceLocalStorageMiddleware and syncWithDatabaseMiddleware to store middleware

--- 09-crud-redux-toolkit/src/store/index.ts
+++ 09-crud-redux-toolkit/src/store/index.ts
@@ -1,17 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, type Middleware } from "@reduxjs/toolkit";
 import usersReducer from "./users/slice.ts";
 
-const persistanceLocalStorageMiddleware = (store) => (next) => (action) => {
-  next(action);
-  localStorage.setItem("users_redux_state", JSON.stringify(store.getState()));
-};
+const persistanceLocalStorageMiddleware: Middleware =
+  (store) => (next) => (action) => {
+    next(action);
+    localStorage.setItem("users_redux_state", JSON.stringify(store.getState()));
+  };
+
+const syncWithDatabaseMiddleware: Middleware =
+  (store) => (next) => (action) => {
+    console.log({ action, state: store.getState() });
+    next(action);
+  };
 
 export const store = configureStore({
   reducer: {
     users: usersReducer
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(persistanceLocalStorageMiddleware)
+    getDefaultMiddleware()
+      .concat(persistanceLocalStorageMiddleware)
+      .concat(syncWithDatabaseMiddleware)
 });
 
 export type RootState = ReturnType<typeof store.getState>;
