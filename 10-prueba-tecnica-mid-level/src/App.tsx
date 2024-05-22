4677913b98614b56d91d9dd15c2160da01e20351
David Almonacid <david.almonacidf@gmail.com>
2024-05-22 15:14:11 -0500

    chore: Add color rows feature to UserList component

--- 10-prueba-tecnica-mid-level/src/App.tsx
+++ 10-prueba-tecnica-mid-level/src/App.tsx
@@ -4,6 +4,11 @@ import UserList from "./components/UserList";
 
 function App() {
   const [users, setUsers] = useState<User[]>([]);
+  const [colorRows, setColorRows] = useState(false);
+
+  const toggleColorRows = () => {
+    setColorRows(!colorRows);
+  };
 
   useEffect(() => {
     fetch("https://randomuser.me/api/?results=100")
@@ -17,9 +22,76 @@ function App() {
   }, []);
 
   return (
-    <>
-      <UserList users={users} />
-    </>
+    <section className="bg-gray-50 dark:bg-gray-950 p-3 sm:p-5">
+      <div className="mx-auto max-w-screen-sm px-4 lg:px-12">
+        <main className="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
+          {/* start header */}
+          <div className="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
+            <div className="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
+              <button
+                type="button"
+                className="flex items-center justify-center text-white bg-gray-700 hover:bg-gray-600 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-gray-600 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
+                onClick={toggleColorRows}
+              >
+                Colorear filas
+              </button>
+              {/* <div className="flex items-center space-x-3 w-full md:w-auto">
+                <button
+                  id="actionsDropdownButton"
+                  data-dropdown-toggle="actionsDropdown"
+                  className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-gray-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                  type="button"
+                >
+                  <svg
+                    className="-ml-1 mr-1.5 w-5 h-5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                  >
+                    <path
+                      clip-rule="evenodd"
+                      fill-rule="evenodd"
+                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                    />
+                  </svg>
+                  Actions
+                </button>
+                <div
+                  id="actionsDropdown"
+                  className="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
+                >
+                  <ul
+                    className="py-1 text-sm text-gray-700 dark:text-gray-200"
+                    aria-labelledby="actionsDropdownButton"
+                  >
+                    <li>
+                      <a
+                        href="#"
+                        className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                      >
+                        Mass Edit
+                      </a>
+                    </li>
+                  </ul>
+                  <div className="py-1">
+                    <a
+                      href="#"
+                      className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                    >
+                      Delete all
+                    </a>
+                  </div>
+                </div>
+              </div> */}
+            </div>
+          </div>
+          {/* end header */}
+
+          <UserList users={users} colorRows={colorRows} />
+        </main>
+      </div>
+    </section>
   );
 }
 
--- 10-prueba-tecnica-mid-level/src/components/UserList.tsx
+++ 10-prueba-tecnica-mid-level/src/components/UserList.tsx
@@ -2,63 +2,58 @@ import type { User } from "../types";
 
 interface Props {
   users: User[];
+  colorRows: boolean;
 }
 
-export default function UserList({ users }: Props) {
+export default function UserList({ users, colorRows }: Props) {
   return (
-    <section className="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5">
-      <div className="mx-auto max-w-screen-sm px-4 lg:px-12">
-        <div className="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
-          <div className="overflow-x-auto">
-            <table className="w-full text-sm text-gray-500 dark:text-gray-400">
-              <thead className="text-xs text-gray-700 text-left uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                <tr>
-                  <th scope="col" className="px-4 py-3">
-                    Foto
-                  </th>
-                  <th scope="col" className="px-4 py-3">
-                    Nombre
-                  </th>
-                  <th scope="col" className="px-4 py-3">
-                    Apellido
-                  </th>
-                  <th scope="col" className="px-4 py-3">
-                    País
-                  </th>
-                  <th scope="col" className="px-4 py-3">
-                    Acciones
-                  </th>
-                </tr>
-              </thead>
+    <div className="overflow-x-auto">
+      <table className="w-full text-sm text-gray-200">
+        <thead className="text-xs text-left uppercase bg-gray-50 dark:bg-gray-700 text-gray-200">
+          <tr>
+            <th scope="col" className="px-4 py-3">
+              Foto
+            </th>
+            <th scope="col" className="px-4 py-3">
+              Nombre
+            </th>
+            <th scope="col" className="px-4 py-3">
+              Apellido
+            </th>
+            <th scope="col" className="px-4 py-3">
+              País
+            </th>
+            <th scope="col" className="px-4 py-3">
+              Acciones
+            </th>
+          </tr>
+        </thead>
 
-              <tbody>
-                {users.map((user, index) => (
-                  <tr
-                    key={user.id.value || index}
-                    className="border-b dark:border-gray-700"
-                  >
-                    <td scope="row" className="px-4 py-3">
-                      <img
-                        className="size-8 rounded-full"
-                        src={user.picture.thumbnail}
-                        alt={`${user.name.first} ${user.name.last} profile picture`}
-                      />
-                    </td>
-                    <td className="px-4 py-3">{user.name.first}</td>
-                    <td className="px-4 py-3">{user.name.last}</td>
-                    <td className="px-4 py-3">{user.location.country}</td>
-                    <td className="px-4 py-3">
-                      <div className="flex">
-                        <button>Borrar</button>
-                      </div>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-    </section>
+        <tbody>
+          {users.map((user, index) => (
+            <tr
+              key={user.id.value ?? index}
+              className={`border-b dark:border-gray-700 ${colorRows ? "odd:bg-gray-600 even:bg-gray-500" : ""}`}
+            >
+              <td scope="row" className="px-4 py-3">
+                <img
+                  className="size-8 rounded-full"
+                  src={user.picture.thumbnail}
+                  alt={`${user.name.first} ${user.name.last} profile picture`}
+                />
+              </td>
+              <td className="px-4 py-3">{user.name.first}</td>
+              <td className="px-4 py-3">{user.name.last}</td>
+              <td className="px-4 py-3">{user.location.country}</td>
+              <td className="px-4 py-3">
+                <div className="flex">
+                  <button>Borrar</button>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
