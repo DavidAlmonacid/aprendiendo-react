589fdf5a60463101140509ab458c30bd51a4a225
David Almonacid <david.almonacidf@gmail.com>
2024-05-22 16:37:03 -0500

    chore: Allow sorting users by country in UserList component

--- 10-prueba-tecnica-mid-level/src/App.tsx
+++ 10-prueba-tecnica-mid-level/src/App.tsx
@@ -5,10 +5,7 @@ import UserList from "./components/UserList";
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [colorRows, setColorRows] = useState(false);
-
-  const toggleColorRows = () => {
-    setColorRows(!colorRows);
-  };
+  const [sortByCountry, setSortByCountry] = useState(false);
 
   useEffect(() => {
     fetch("https://randomuser.me/api/?results=100")
@@ -20,6 +17,20 @@ function App() {
         console.error(error);
       });
   }, []);
+
+  const toggleColorRows = () => {
+    setColorRows(!colorRows);
+  };
+
+  const toggleSortByCountry = () => {
+    setSortByCountry((prevState) => !prevState);
+  };
+
+  const sortedUsers = sortByCountry
+    ? [...users].sort((a, b) =>
+        a.location.country.localeCompare(b.location.country)
+      )
+    : users;
 
   return (
     <section className="bg-gray-50 dark:bg-gray-950 p-3 sm:p-5">
@@ -35,60 +46,19 @@ function App() {
               >
                 Colorear filas
               </button>
-              {/* <div className="flex items-center space-x-3 w-full md:w-auto">
-                <button
-                  id="actionsDropdownButton"
-                  data-dropdown-toggle="actionsDropdown"
-                  className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-gray-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                  type="button"
-                >
-                  <svg
-                    className="-ml-1 mr-1.5 w-5 h-5"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                    aria-hidden="true"
-                  >
-                    <path
-                      clip-rule="evenodd"
-                      fill-rule="evenodd"
-                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                    />
-                  </svg>
-                  Actions
-                </button>
-                <div
-                  id="actionsDropdown"
-                  className="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
-                >
-                  <ul
-                    className="py-1 text-sm text-gray-700 dark:text-gray-200"
-                    aria-labelledby="actionsDropdownButton"
-                  >
-                    <li>
-                      <a
-                        href="#"
-                        className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                      >
-                        Mass Edit
-                      </a>
-                    </li>
-                  </ul>
-                  <div className="py-1">
-                    <a
-                      href="#"
-                      className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                    >
-                      Delete all
-                    </a>
-                  </div>
-                </div>
-              </div> */}
+
+              <button
+                type="button"
+                className="flex items-center justify-center text-white focus:ring-4 font-medium rounded-lg text-sm px-4 py-2 bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-gray-800"
+                onClick={toggleSortByCountry}
+              >
+                {sortByCountry ? "No ordenar" : "Ordenar"} por país
+              </button>
             </div>
           </div>
           {/* end header */}
 
-          <UserList users={users} colorRows={colorRows} />
+          <UserList users={sortedUsers} colorRows={colorRows} />
         </main>
       </div>
     </section>
