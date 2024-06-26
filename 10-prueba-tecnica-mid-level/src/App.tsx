cd2cbc5969f708eb24ef7b55050c93b26d913a6b
David Almonacid <david.almonacidf@gmail.com>
2024-06-26 17:47:17 -0500

    refactor: Update App component to use infinite query for fetching and managing users

--- 10-prueba-tecnica-mid-level/src/App.tsx
+++ 10-prueba-tecnica-mid-level/src/App.tsx
@@ -1,61 +1,27 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { useMemo, useState } from "react";
+import { fetchUsers } from "./api";
 import UserList from "./components/UserList";
 import { SortBy, type User } from "./types";
 
-async function fetchUsers({ page }: { page: number }): Promise<User[]> {
-  const response = await fetch(
-    `https://randomuser.me/api/?results=10&seed=foobar2&page=${page}`
-  );
-
-  if (!response.ok) {
-    throw new Error("Error en la petición");
-  }
-
-  const { results } = await response.json();
-  return results;
-}
-
 function App() {
-  const query = useQuery({
+  const query = useInfiniteQuery({
     queryKey: ["users"],
-    queryFn: async () => await fetchUsers({ page: 1 })
+    queryFn: ({ pageParam }) => fetchUsers({ page: pageParam }),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage) => lastPage.nextPage
   });
 
-  const { data: users = [], isLoading, isError, refetch } = query;
+  const { data, isLoading, isError, refetch, fetchNextPage } = query;
+  console.log(data);
 
-  // const [users, setUsers] = useState<User[]>([]);
+  const users: User[] = data?.pages.flatMap((page) => page.users) ?? [];
+
   // const originalUsers = useRef<User[]>([]);
 
   const [colorRows, setColorRows] = useState(false);
   const [sorting, setSorting] = useState<SortBy>(SortBy.None);
   const [filterByCountry, setFilterByCountry] = useState("");
-
-  // const [loading, setLoading] = useState(true);
-  // const [error, setError] = useState(false);
-  const [page, setPage] = useState(1);
-
-  // useEffect(() => {
-  //   setLoading(true);
-  //   setError(false);
-
-  //   fetchUsers({ page })
-  //     .then((users) => {
-  //       setUsers((prevUsers) => {
-  //         const newUsers = prevUsers.concat(users);
-  //         originalUsers.current = newUsers;
-
-  //         return newUsers;
-  //       });
-  //     })
-  //     .catch((error) => {
-  //       setError(true);
-  //       console.error(error);
-  //     })
-  //     .finally(() => {
-  //       setLoading(false);
-  //     });
-  // }, [page]);
 
   const toggleColorRows = () => {
     setColorRows(!colorRows);
@@ -183,7 +149,7 @@ function App() {
           <div className="flex justify-center mt-4">
             <button
               className="bg-gray-700 text-white font-medium rounded-lg text-sm px-4 py-2"
-              onClick={() => setPage(page + 1)}
+              onClick={() => fetchNextPage()}
             >
               Cargar más resultados
             </button>
