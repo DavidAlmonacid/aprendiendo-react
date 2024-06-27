bf0bb2f7f8db2f7677fea3d0a6b4cfd34aedac96
David Almonacid <david.almonacidf@gmail.com>
2024-06-27 17:54:49 -0500

    refactor: Add config file

---
+++ 11-comments-react-query/src/config.ts
@@ -0,0 +1,3 @@
+export const API_KEY = import.meta.env.VITE_JSON_BIN_API_KEY;
+export const BASE_URL = import.meta.env.VITE_JSON_BIN_ROOT_URL;
+export const BIN_ID = import.meta.env.VITE_JSON_BIN_ID;
