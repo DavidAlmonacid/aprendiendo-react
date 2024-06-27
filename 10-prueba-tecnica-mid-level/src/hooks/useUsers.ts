c6dcfe86064a237960b4c8cfdf6cca5ea5e1ae20
David Almonacid <david.almonacidf@gmail.com>
2024-06-27 12:18:01 -0500

    refactor: Add useUsers hook for fetching and managing users

---
+++ 10-prueba-tecnica-mid-level/src/hooks/useUsers.ts
@@ -0,0 +1,21 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { fetchUsers } from "../api/users";
+
+export function useUsers() {
+  const { data, isLoading, isError, hasNextPage, refetch, fetchNextPage } =
+    useInfiniteQuery({
+      queryKey: ["users"],
+      queryFn: ({ pageParam }) => fetchUsers({ page: pageParam }),
+      initialPageParam: 1,
+      getNextPageParam: (lastPage) => lastPage.nextPage
+    });
+
+  return {
+    data,
+    isLoading,
+    isError,
+    hasNextPage,
+    refetch,
+    fetchNextPage
+  };
+}
