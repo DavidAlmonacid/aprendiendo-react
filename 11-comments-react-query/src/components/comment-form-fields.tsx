1d5b25b810c27fead3576434663a331a4caeab11
David Almonacid <david.almonacidf@gmail.com>
2024-06-27 17:55:55 -0500

    refactor: Add comment form fields and comments list components

---
+++ 11-comments-react-query/src/components/comment-form-fields.tsx
@@ -0,0 +1,26 @@
+export function FormInput() {
+  return (
+    <div className="mb-6">
+      <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+        <span>Título del comentario</span>
+      </label>
+      <input
+        type="text"
+        name="title"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        placeholder="Título..."
+      />
+    </div>
+  );
+}
+
+export function FormTextArea() {
+  return (
+    <textarea
+      name="message"
+      rows={4}
+      className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+      placeholder="Quería comentar que..."
+    />
+  );
+}
