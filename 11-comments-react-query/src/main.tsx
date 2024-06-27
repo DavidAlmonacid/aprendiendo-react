db8ca1631ba91e218767457730d4719c898a95e8
David Almonacid <david.almonacidf@gmail.com>
2024-06-27 17:55:29 -0500

    refactor: Update main.tsx to use @tanstack/react-query for data fetching

--- 11-comments-react-query/src/main.tsx
+++ 11-comments-react-query/src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+import App from "./App.tsx";
+import "./index.css";
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
     <App />
-  </React.StrictMode>,
-)
+  </QueryClientProvider>
+);
