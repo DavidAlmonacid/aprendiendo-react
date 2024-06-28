e695a9966e31c594985747d18a4ff193449daf70
David Almonacid <david.almonacidf@gmail.com>
2024-06-28 18:13:21 -0500

    refactor: Update App.tsx with improved layout and components

--- 12-javascript-quiz-zustand/src/App.tsx
+++ 12-javascript-quiz-zustand/src/App.tsx
@@ -1,7 +1,27 @@
+import { Container, Stack, Typography } from "@mui/material";
+import { PythonIcon } from "./components/icons";
+import { StartButton } from "./components/start-button";
+
 export default function App() {
   return (
-    <>
-      <h1 className="text-xl">JavaScript Quiz Zustand</h1>
-    </>
+    <main>
+      <Container maxWidth="sm">
+        <Stack direction="column" spacing={4} alignItems="center">
+          <Stack
+            direction="row"
+            spacing={2}
+            alignItems="center"
+            justifyContent="center"
+          >
+            <PythonIcon />
+            <Typography variant="h3" component="h1" fontWeight={700}>
+              JavaScript Quiz
+            </Typography>
+          </Stack>
+
+          <StartButton />
+        </Stack>
+      </Container>
+    </main>
   );
 }
