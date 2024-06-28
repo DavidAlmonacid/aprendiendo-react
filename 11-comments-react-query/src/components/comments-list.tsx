2ff0d38c264f8c3bafdeed52d3634efab00a6ec7
David Almonacid <david.almonacidf@gmail.com>
2024-06-28 15:30:19 -0500

    refactor: Improve styles

--- 11-comments-react-query/src/components/comments-list.tsx
+++ 11-comments-react-query/src/components/comments-list.tsx
@@ -8,7 +8,7 @@ export function CommentsList({ data }: { data?: Comment[] }) {
           <article
             key={comment.id}
             className={`
-            ${comment.preview === true ? "bg-gray-400" : "bg-white"} block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100`}
+            ${comment.preview ? "bg-stone-300 opacity-80" : "bg-white"} block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100`}
           >
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
               {comment.title}
