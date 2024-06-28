a205e0dcc58e0c06962a76d03265480120f2c5b0
David Almonacid <david.almonacidf@gmail.com>
2024-06-28 10:16:40 -0500

    refactor: Add required attribute to comment form fields

--- 11-comments-react-query/src/components/comment-form-fields.tsx
+++ 11-comments-react-query/src/components/comment-form-fields.tsx
@@ -9,6 +9,7 @@ export function FormInput() {
         name="title"
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
         placeholder="Título..."
+        required
       />
     </div>
   );
@@ -21,6 +22,7 @@ export function FormTextArea() {
       rows={4}
       className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
       placeholder="Quería comentar que..."
+      required
     />
   );
 }
