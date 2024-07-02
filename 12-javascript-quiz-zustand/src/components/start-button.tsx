c2af2081300a3bcf56032b236a1c4aee0e7b7cbf
David Almonacid <david.almonacidf@gmail.com>
2024-07-02 12:24:48 -0500

    feat: Add global state function for fetching the questions

--- 12-javascript-quiz-zustand/src/components/start-button.tsx
+++ 12-javascript-quiz-zustand/src/components/start-button.tsx
@@ -1,8 +1,15 @@
 import { Button } from "@mui/material";
+import { useQuestionsStore } from "../stores/questions";
 
 export function StartButton() {
+  const fetchQuestions = useQuestionsStore((state) => state.fetchQuestions);
+
+  const handleClick = () => {
+    fetchQuestions(5);
+  };
+
   return (
-    <Button variant="contained" color="primary">
+    <Button variant="contained" color="primary" onClick={handleClick}>
       ¡Comenzar!
     </Button>
   );
