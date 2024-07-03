71fbeefc070c14e44a6ee443e1dc04e275f4aec1
David Almonacid <david.almonacidf@gmail.com>
2024-07-03 11:25:49 -0500

    refactor: Update Game component to use MUI Typography and Box components for question rendering

--- 12-javascript-quiz-zustand/src/components/game.tsx
+++ 12-javascript-quiz-zustand/src/components/game.tsx
@@ -1,4 +1,4 @@
-import { Card, Typography } from "@mui/material";
+import { Box, Card, Typography } from "@mui/material";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { gradientDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
@@ -8,13 +8,21 @@ import type { Question as QuestionType } from "../types";
 function Question({ question }: { question: QuestionType }) {
   return (
     <Card variant="outlined">
-      <Typography variant="h5" component="h2">
+      <Typography
+        variant="h6"
+        component="h2"
+        lineHeight={1.5}
+        padding={1}
+        marginBottom={1}
+      >
         {question.question}
       </Typography>
 
-      <SyntaxHighlighter language={question.language} style={gradientDark}>
-        {question.code}
-      </SyntaxHighlighter>
+      <Box fontSize={14}>
+        <SyntaxHighlighter language={question.language} style={gradientDark}>
+          {question.code}
+        </SyntaxHighlighter>
+      </Box>
     </Card>
   );
 }
