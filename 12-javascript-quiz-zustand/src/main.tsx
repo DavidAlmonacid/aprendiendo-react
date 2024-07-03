53ec6c97dc28f3e5dab5b9705b9f1b3012289205
David Almonacid <david.almonacidf@gmail.com>
2024-07-03 11:24:05 -0500

    refactor: Update main.tsx to use MUI dark theme

--- 12-javascript-quiz-zustand/src/main.tsx
+++ 12-javascript-quiz-zustand/src/main.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
@@ -9,8 +12,17 @@ import "@fontsource/roboto/700.css";
 import App from "./App.tsx";
 import "./index.css";
 
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark"
+  }
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
