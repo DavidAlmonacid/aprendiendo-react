5e45fc0f8f3f3e9cb14a79fdc4e79ad76b164c9d
David Almonacid <david.almonacidf@gmail.com>
2024-07-09 11:29:00 -0500

    refactor: Add previous and next question navigation buttons in Game component

--- 12-javascript-quiz-zustand/src/components/game.tsx
+++ 12-javascript-quiz-zustand/src/components/game.tsx
@@ -1,3 +1,5 @@
+import { ArrowBackIosNew } from "@mui/icons-material";
+import { Box, IconButton, Stack } from "@mui/material";
 import { useQuestionsStore } from "../stores/questions";
 import { Question } from "./question";
 
@@ -6,12 +8,32 @@ export function Game() {
   const currentQuestionIndex = useQuestionsStore(
     (state) => state.currentQuestionIndex
   );
+  const goPreviousQuestion = useQuestionsStore(
+    (state) => state.goPreviousQuestion
+  );
+  const goNextQuestion = useQuestionsStore((state) => state.goNextQuestion);
 
   const currentQuestion = questions[currentQuestionIndex];
 
   return (
-    <div>
+    <Box sx={{ margin: "0 !important" }}>
+      <Stack direction="row" justifyContent="center" marginY="8px">
+        <IconButton
+          onClick={goPreviousQuestion}
+          disabled={currentQuestionIndex === 0}
+        >
+          <ArrowBackIosNew sx={{ paddingRight: "3px" }} />
+        </IconButton>
+
+        <IconButton
+          onClick={goNextQuestion}
+          disabled={currentQuestionIndex === questions.length - 1}
+        >
+          <ArrowBackIosNew sx={{ paddingRight: "3px", rotate: "180deg" }} />
+        </IconButton>
+      </Stack>
+
       <Question question={currentQuestion} />
-    </div>
+    </Box>
   );
 }
